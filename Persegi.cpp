f359a821e7cc771dde2fed67a35bb549aff902c7
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-02-24 16:07:30 +0700

    CHANGES

---
+++ Persegi.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // start
+    float p, l, luas;
+
+    // langkah 2 : Masukkan Panjang
+    cout << "Masukkan Panjang : ";
+    cin >> p;
+
+    // langkah 3 : Masukkan Lebar
+    cout << "Masukkan Lebar : ";
+    cin >> l;
+
+    // langkah 4 : Hitung Luas = p x l
+    luas = p * l;
+
+    // langkah 5 :
+    cout << "Hasilnya = " << luas;
+
+    // selesai
+    return 0;
+}
---
+++ coba2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int angka = 10;
+    float tinggi = 180.5;
+    double harga = 200000.00;
+    string nama = "Aldi";
+
+    cout << "Selamat Datang" << endl;
+    cout << "di TI UMY\n";
+    cout << "Yogyakarta\n";
+
+    nama = "Abra";
+    cout << "Nama saya : " << nama;
+
+    return 0;
+}
