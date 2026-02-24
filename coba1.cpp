60535c2a6633a39d2197e28dd57904b83091343c
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-02-24 15:51:13 +0700

    first commit : menambahkan file coba1.cpp

---
+++ coba1.cpp
@@ -0,0 +1,19 @@
+//deklarasi library
+#include<iostream>
+using namespace std;
+
+//program utama
+int main()
+{
+// deklarasi variabel umur
+int umur;
+//memberi nilai dari variabel
+umur = 20;
+
+//menampilkan isi string
+cout << "Abra" << endl;
+//menampilkan isi integer
+cout << "78" << endl;
+//menampilkan isi dari variabel cout
+cout << umur << endl;
+}
